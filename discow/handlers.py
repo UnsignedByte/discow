45c7b9431a57da9169601cb78e172898b01f2e6e
Maydoh1239 <34429084+UnsignedByte@users.noreply.github.com>
2018-03-25 17:29:01 -0700

    push the mad

--- discow/handlers.py
+++ discow/handlers.py
@@ -7,12 +7,18 @@ def test_message(Discow, msg):
     tmp = yield from Discow.send_message(msg.channel, "Hi **%s**! I'm Discow." % msg.author.name)
 
 @asyncio.coroutine
-def test_heart(Discow, msg):
-    tmp = yield from Discow.send_message(msg.channel, "I love you too, **%s**! :heart:" % msg.author.name)
+def rps(Discow, msg):
+    mine = "rock"
+    try:
+        tmp = yield from Discow.send_message(msg.channel, format_response("**{_mention}** chooses **%s**, while I choose **%s**." % (msg.content.split(" ")[1], mine)))
+    except IndexError:
+        tmp = yield from Discow.send_message(msg.channel, "Please provide an input.")
+
 
 message_handlers = {
     # Format is (command, function)
-    "test": test_message
+    "test": test_message,
+    "rps": rps
 }
 
 discow_prefix = "&"
@@ -34,7 +40,6 @@ def on_message(Discow, msg):
     if msg.content[:len(discow_prefix)] != discow_prefix:
         return
     try:
-        print(get_command(msg.content[len(discow_prefix):].split(" ")[0]))
         yield from message_handlers[get_command(msg.content[len(discow_prefix)-1:].split(" ")[0])](Discow, msg)
     except KeyError:
         tmp = yield from Discow.send_message(msg.channel, "Unknown command **%s**." % get_command(msg.content))
