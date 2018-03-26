68ec77fa8f5d3ac99185d4be08cf9eb903dd6048
Maydoh1239 <34429084+UnsignedByte@users.noreply.github.com>
2018-03-26 07:09:14 -0700

    Default schedule handling

--- discow/gunn_schedule/calendar_.py
+++ discow/gunn_schedule/calendar_.py
@@ -1,6 +1,7 @@
 from discow.handlers import message_handlers
 import asyncio
 import discow.utils
+import re
 
 import datetime
 import dateparser as cf
@@ -24,6 +25,62 @@ def specialParseDate(string):
     string = string.replace(' ', '')
     if string.startswith("this") or string.startswith("next"):
         return parseDate(string[4:]) + datetime.timedelta(7)
+
+# credit to https://stackoverflow.com/a/13756038
+def td_format(td_object):
+    seconds = int(td_object.total_seconds())
+    periods = [
+        ('year',        60*60*24*365),
+        ('month',       60*60*24*30),
+        ('day',         60*60*24),
+        ('hour',        60*60),
+        ('minute',      60),
+        ('second',      1)
+    ]
+
+    strings=[]
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value , seconds = divmod(seconds, period_seconds)
+            has_s = 's' if period_value > 1 else ''
+            strings.append("%s %s%s" % (period_value, period_name, has_s))
+
+    return ", ".join(strings)
+
+class ScheduleEvent:
+    def __init__(self, starttime, endtime, name, desc):
+        self.start = starttime
+        self.end = endtime
+        self.name = name
+        self.desc = desc
+    def format(self, tfhour = False, showlengths = True):
+        return "%s: **%s** - **%s** " % (self.name,
+                                             self.start.time().strftime("%I:%M"),
+                                             self.end.time().strftime("%I:%M")) + "(%s)" % (td_format(self.end-self.start))
+
+defaults = {}
+raw_defaults = open("discow/gunn_schedule/defaults.txt").read().split('\n')
+curr_day_text = None
+curr_schedule = []
+
+getName = re.compile('\[(.+)\]')
+getTime = re.compile('\((.+)\)')
+
+for sched in raw_defaults:
+    if sched.startswith("$$"):
+        defaults[curr_day_text] = curr_schedule.copy()
+        curr_day_text = sched[2:]
+        curr_schedule = []
+    else:
+        if sched == "None":
+            continue
+        elif sched.startswith('!e'):
+            name = getName.findall(sched)[0]
+            times = getTime.findall(sched)[0].split('-')
+            curr_schedule.append(ScheduleEvent(parseSchoolDate(times[0]), parseSchoolDate(times[1]),
+                                               name, None))
+defaults[curr_day_text] = curr_schedule.copy()
+
 
 @asyncio.coroutine
 def calendar(Discow, msg):
@@ -49,7 +106,10 @@ def calendar(Discow, msg):
                 hasevent = True
     g.close()
     if not hasevent:
-        out+="\nNo events scheduled for today."
+        relev  = defaults[str(parsed.weekday())]
+        for event in relev:
+            out+="\n"+event.format()
+
     yield from Discow.send_message(msg.channel, out)
 
 message_handlers["cal"] = calendar
