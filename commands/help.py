f030bc7b82de853ca21a66ee6d35e44787b3e9d1
Edmund Lam <edlam1889@gmail.com>
2018-04-03 21:33:19 -0700

    Fix help size

--- commands/help.py
+++ commands/help.py
@@ -22,29 +22,35 @@ with open("README.md", "r") as f:
             elif l.startswith("|") and l.endswith("|"):
                 cmd = list(map(lambda x:x.replace('`', ""), l.strip("|").split("|")))
                 helpvals[lastheader].append("< "+cmd[1]+" >\n[ AKA "+cmd[3].ljust(15)+" ]( "+cmd[2]+" )")
-                #helpvals[lastheader].append("**"+cmd[0].title()+"**: "+cmd[2]+" Usage: "+cmd[1]+". Name(s): "+cmd[3]+".")
             else:
                 helpvals[lastheader].append('> '+l.replace('`', ''))
 
-em = Embed(colour=0x9542f4)
+helpembed = Embed(colour=0x9542f4)
+
+desc = "```markdown"
 
 first = True
 for key, value in helpvals.items():
     if first:
-        em.title = key
-        em.description = '\n'.join(map(lambda x:x.lstrip(">").strip(), value))+"\n\n```markdown"
+        helpembed.title = key
+        helpembed.description = '\n'.join(map(lambda x:x.lstrip(">").strip(), value))
         first = False
     else:
-        em.description+="\n\n# "+key+'\n\n'+'\n'.join(value)
-em.description+='\n```'
-
+        stoadd = "\n\n# "+key+'\n\n'+'\n'.join(value)
+        if len(desc)+len(stoadd) >= 1000:
+            print(desc)
+            helpembed.add_field(name="\a", value=desc+'```')
+            desc = "```markdown"+stoadd
+        else:
+            desc+=stoadd
+helpembed.add_field(name="\a", value=desc+'```')
 print("Finished Parsing")
 
 @asyncio.coroutine
 def gethelp(Discow, msg):
     myinfo = yield from Discow.application_info()
     me = yield from Discow.get_user_info(myinfo.id)
-    em.set_footer(text="Created by "+me.display_name+" on "+get_localized_time(msg.server)+".", icon_url=myinfo.icon_url)
-    yield from Discow.send_message(msg.channel, embed=em)
+    helpembed.set_footer(text="Created by "+me.display_name+" on "+get_localized_time(msg.server)+".", icon_url=myinfo.icon_url)
+    yield from Discow.send_message(msg.channel, embed=helpembed)
 
 add_message_handler(gethelp, "help")
