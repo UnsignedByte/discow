b4bd39d2881d14a13412ba06db315cb7101fbfe9
UnsignedByte <edlam1889@gmail.com>
2018-11-06 22:50:09 -0800

    move whoping outside to read for bot pings

--- discow/handlers.py
+++ discow/handlers.py
@@ -2,7 +2,7 @@
 # @Date:   06:50:24, 02-May-2018
 # @Filename: handlers.py
 # @Last modified by:   edl
-# @Last modified time: 22:47:35, 06-Nov-2018
+# @Last modified time: 22:49:22, 06-Nov-2018
 
 bot_data = {}
 discow_prefix = "cow "
@@ -92,6 +92,16 @@ import re
 import asyncio
 
 async def on_message(Bot, msg):
+    if msg.role_mentions or msg.mention_everyone:
+        for m in msg.server.members:
+            if not m.bot and m.mentioned_in(msg):
+                datautils.nested_set(msg, 'user_data', m.id, 'last_mention')
+        await message_handlers["save"](Bot, msg, overrideperms=True)
+    else:
+        for m in msg.mentions:
+            if not m.bot:
+                datautils.nested_set(msg, 'user_data', m.id, 'last_mention')
+        await message_handlers["save"](Bot, msg, overrideperms=True)
     if not msg.author.bot:
         if not msg.content.startswith(discow_prefix):
             for a in regex_message_handlers:
@@ -99,16 +109,6 @@ async def on_message(Bot, msg):
                 if reg:
                     await regex_message_handlers[a](Bot, msg, reg)
                     break
-            if msg.role_mentions or msg.mention_everyone:
-                for m in msg.server.members:
-                    if not m.bot and m.mentioned_in(msg):
-                        datautils.nested_set(msg, 'user_data', m.id, 'last_mention')
-                await message_handlers["save"](Bot, msg, overrideperms=True)
-            else:
-                for m in msg.mentions:
-                    if not m.bot:
-                        datautils.nested_set(msg, 'user_data', m.id, 'last_mention')
-                await message_handlers["save"](Bot, msg, overrideperms=True)
             if Bot.user in msg.mentions:
                 await Bot.send_message(msg.channel, "Type `cow help` if you need help.")
             return
