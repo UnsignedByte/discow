6c101977d410f47112fde1a3e8ed044f48f035ab
UnsignedByte <edlam1889@gmail.com>
2018-11-11 08:05:10 -0800

    fix uutls

--- utils/userutils.py
+++ utils/userutils.py
@@ -2,12 +2,13 @@
 # @Date:   20:17:56, 04-Nov-2018
 # @Filename: memberutils.py
 # @Last modified by:   edl
-# @Last modified time: 20:19:40, 04-Nov-2018
+# @Last modified time: 08:05:02, 11-Nov-2018
 
 import asyncio
+from utils import datautils
 
 async def get_owner(Bot):
     return (await Bot.application_info()).owner
 
 async def is_mod(Bot, user):
-    return user == (await get_owner(Bot)) or user in nested_get('global_data', 'moderators')
+    return user == (await get_owner(Bot)) or user in datautils.nested_get('global_data', 'moderators')
