ed4a2ea2a58c70a8093379409fa179b021db4a8e
UnsignedByte <edlam1889@gmail.com>
2018-11-11 08:59:22 -0800

    edmund is dumb

--- utils/userutils.py
+++ utils/userutils.py
@@ -2,7 +2,7 @@
 # @Date:   20:17:56, 04-Nov-2018
 # @Filename: memberutils.py
 # @Last modified by:   edl
-# @Last modified time: 08:56:38, 11-Nov-2018
+# @Last modified time: 08:59:15, 11-Nov-2018
 
 import asyncio
 from utils import datautils
@@ -15,7 +15,7 @@ async def is_mod(Bot, user):
     return user == (await get_owner(Bot)) or user in datautils.nested_get('global_data', 'moderators')
 
 def get_user_color(user):
-    if isinstance(user, discord.User):
-        return 0x3a71c1;
-    else:
+    if isinstance(user, discord.Member):
         return user.colour
+    else:
+        return 0x3a71c1
